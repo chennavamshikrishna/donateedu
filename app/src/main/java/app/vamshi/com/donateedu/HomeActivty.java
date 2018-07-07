ebf9fb85aa32e90275b9b89d87daf50871e610aa
vamshi <chennavamshikrishna69@gmail.com>
2018-07-07 16:37:31 +0530

    login pages commit

--- app/src/main/java/app/vamshi/com/donateedu/HomeActivty.java
+++ app/src/main/java/app/vamshi/com/donateedu/HomeActivty.java
@@ -1,32 +1,31 @@
 package app.vamshi.com.donateedu;
 
-import android.content.Intent;
 import android.os.Build;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
-public class LoginActivty extends AppCompatActivity {
+public class HomeActivty extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_activty);
+        setContentView(R.layout.activity_home_activty);
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        DrawerLayout drawerLayout=findViewById(R.id.drawerlayout);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.setStatusBarColor(getResources().getColor(R.color.material_blue));
         }
-        TextView signup=findViewById(R.id.newuser);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivty.this,SignupActivity.class));
-            }
-        });
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
 
     }
 }
